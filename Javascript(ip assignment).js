8b80b4950f7bc3bd58a46ca27e420efdbb1cfe16
Abhishek007-cracker <83574979+Abhishek007-cracker@users.noreply.github.com>
2022-09-17 18:00:29 +0530

    Add files via upload

---
+++ Javascript(ip assignment).js
@@ -0,0 +1,12 @@
+t=[5,21,38,95]
+for (let i = 0; i < t.length; i++) {
+    const element = t[i];
+    console.log(t[i]);
+}
+var p=prompt("Enter:");
+if(p>10){
+    console.log("Great");
+}
+else{
+    console.log("okay");
+}
---
+++ iterators.js
@@ -0,0 +1,12 @@
+obj={
+    name:"Harry",
+    language:"Javascript",
+    hobbies:"Android and web development"
+
+}
+for (const key in obj) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    }
+}
